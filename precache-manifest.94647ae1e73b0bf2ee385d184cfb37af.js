c685fa4acea4eb6a3ddcf1bfd815b87ba59f99c9
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-07-22 14:05:01 -0300

    Updates

---
+++ precache-manifest.94647ae1e73b0bf2ee385d184cfb37af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa80261e6e99e34b738460d19fe8188b",
+    "url": "/game-react-redux/index.html"
+  },
+  {
+    "revision": "b7f6f2786f41707e5e2e",
+    "url": "/game-react-redux/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "f09a4def57252b062d3a",
+    "url": "/game-react-redux/static/css/main.dbd99f2e.chunk.css"
+  },
+  {
+    "revision": "b7f6f2786f41707e5e2e",
+    "url": "/game-react-redux/static/js/2.7399964c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-react-redux/static/js/2.7399964c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09a4def57252b062d3a",
+    "url": "/game-react-redux/static/js/main.0d721816.chunk.js"
+  },
+  {
+    "revision": "6544f6a95f6ef4e4264c",
+    "url": "/game-react-redux/static/js/runtime-main.1de6bda1.js"
+  },
+  {
+    "revision": "486a5d5c038fe6d85ec84a00b63c87ad",
+    "url": "/game-react-redux/static/media/playerImage.486a5d5c.jpg"
+  }
+]);
